bd3d3cc5f332b9b7e16bc003be850bda2ca42018
joonalai <joona.laine@spatineo.com>
2019-04-29 14:14:43 +0300

    Remove test bean

--- src/main/java/fi/fmi/avi/archiver/AviationMessageArchiver.java
+++ src/main/java/fi/fmi/avi/archiver/AviationMessageArchiver.java
@@ -1,6 +1,5 @@
 package fi.fmi.avi.archiver;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
@@ -22,12 +21,4 @@ public class AviationMessageArchiver {
     public ThreadGroup aviationMessageServiceThreadGroup() {
         return new ThreadGroup(AviationMessageArchiver.class.getSimpleName());
     }
-
-    // TODO: Remove this
-    @Bean
-    public String testBean(@Value("${test.value}") final String value) {
-        System.out.println("Running module: " + value);
-        return value;
-    }
-
 }
