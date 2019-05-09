aeb0da5a70cc91b7a33407947bb0fb73530c8c8b
kuoremak <vP#TulevaKesa22>
2019-05-09 16:53:38 +0300

    SDM-1502, added archiveProcessor and read the contents of the file

--- src/main/java/fi/fmi/avi/archiver/config/DirectoryInspectionConfig.java
+++ src/main/java/fi/fmi/avi/archiver/config/DirectoryInspectionConfig.java
@@ -1,5 +1,13 @@
 package fi.fmi.avi.archiver.config;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.nio.charset.Charset;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -11,6 +19,7 @@ import org.springframework.integration.dsl.Pollers;
 import org.springframework.integration.dsl.context.IntegrationFlowContext;
 import org.springframework.integration.scheduling.PollerMetadata;
 import org.springframework.messaging.MessageChannel;
+import org.springframework.util.FileCopyUtils;
 
 import fi.fmi.avi.archiver.initializing.AviationProductsHolder;
 import fi.fmi.avi.archiver.initializing.MessageFileMonitorInitializer;
@@ -47,8 +56,29 @@ public class DirectoryInspectionConfig {
     @Bean
     public IntegrationFlow fileProcessor() {
         return IntegrationFlows.from(processingChannel)//
+                .transform(this::fileToStringTransformer)
                 .channel(archivedChannel)//
                 .get();
     }
+
+    private String fileToStringTransformer(final File file) {
+        try {
+            Reader reader = new BufferedReader(new InputStreamReader(new FileInputStream(file), Charset.defaultCharset()));
+            return FileCopyUtils.copyToString(reader);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @Bean
+    public IntegrationFlow archiveProcessor() {
+        return IntegrationFlows.from(archivedChannel)
+                //Only testing
+                .transform(String.class, str -> {System.out.println(str);return str.toUpperCase();})
+                .log("Archive")
+                .get();
+    }
+
 
 }
