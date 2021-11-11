0f140874bf7a7d613a5c210384045c7a5d1ebf86
kasarit <49980037+kasarit@users.noreply.github.com>
2021-11-11 14:49:22 +0200

    #51: Added database assertions to AviationMessageArchiverTest

--- src/test-integration/resources/h2-data/avidb_test_content.sql
+++ src/test-integration/resources/h2-data/avidb_test_content.sql
@@ -15,5 +15,15 @@ INSERT INTO public.avidb_message_types
 VALUES (3, 'TAF', 'taf', '2021-06-03 00:00:00', 1);
 
 INSERT INTO public.avidb_stations
-VALUES (1, 'EFXX', 'Test Airport', NULL, 1, '1700-01-01 00:00:00', '9999-12-31 00:00:00', '2021-06-03 00:00:00', NULL,
+VALUES (1, 'EFXX', 'EFXX Test Airport', NULL, 1, '1700-01-01 00:00:00', '9999-12-31 00:00:00', '2021-06-03 00:00:00', NULL,
         'US');
+INSERT INTO public.avidb_stations
+VALUES (2, 'YUDO', 'YUDO Test Airport', NULL, 1, '1700-01-01 00:00:00', '9999-12-31 00:00:00', '2021-06-03 00:00:00', NULL,
+        'US');
+INSERT INTO public.avidb_stations
+VALUES (3, 'YUDD', 'YUDD Test Airport', NULL, 1, '1700-01-01 00:00:00', '9999-12-31 00:00:00', '2021-06-03 00:00:00', NULL,
+        'US');
+INSERT INTO public.avidb_stations
+VALUES (4, 'EETN', 'EETN Test Airport', NULL, 1, '1700-01-01 00:00:00', '9999-12-31 00:00:00', '2021-06-03 00:00:00', NULL,
+        'US');
+
