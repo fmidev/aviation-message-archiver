4a5bf8e7e72896bbd8584d2f3bca1e700a83673d
Petri Vuorio <21276470+petringo@users.noreply.github.com>
2022-03-28 14:13:36 +0300

    Fix: apply FilenameMatcher time zone when completing 2-digit year to full year.

--- src/test/java/fi/fmi/avi/archiver/file/FilenameMatcherTest.java
+++ src/test/java/fi/fmi/avi/archiver/file/FilenameMatcherTest.java
@@ -1,42 +1,31 @@
 package fi.fmi.avi.archiver.file;
 
-import fi.fmi.avi.model.PartialOrCompleteTimeInstant;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import java.time.Clock;
 import java.time.Instant;
-import java.time.ZoneId;
+import java.time.ZoneOffset;
 import java.time.ZonedDateTime;
 import java.util.Optional;
 import java.util.regex.Pattern;
 import java.util.stream.Stream;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import fi.fmi.avi.model.PartialOrCompleteTimeInstant;
 
 public class FilenameMatcherTest {
+    private static final Clock CLOCK = Clock.fixed(Instant.parse("2022-02-10T00:00:00Z"), ZoneOffset.UTC);
 
-    private static final ZoneId UTC = ZoneId.of("Z");
-    private static final Clock CLOCK = Clock.fixed(Instant.parse("2022-02-10T00:00:00Z"), UTC);
+    private static final Pattern FOUR_DIGIT_YEAR = Pattern.compile(
+            "^TAF_(?<dd>\\d{2})(?<MM>\\d{2})(?<yyyy>\\d{4})_(?<hh>\\d{2})(?<mm>\\d{2})(?<ss>\\d{2})_\\d{8}\\.xml$");
+    private static final Pattern TWO_DIGIT_YEAR = Pattern.compile(
+            "^TAF_(?<dd>\\d{2})(?<MM>\\d{2})(?<yy>\\d{2})_(?<hh>\\d{2})(?<mm>\\d{2})(?<ss>\\d{2})_\\d{8}\\.xml$");
 
-    private static final Pattern FOUR_DIGIT_YEAR = Pattern
-            .compile("^TAF_(?<dd>\\d{2})(?<MM>\\d{2})(?<yyyy>\\d{4})_(?<hh>\\d{2})(?<mm>\\d{2})(?<ss>\\d{2})_\\d{8}\\.xml$");
-    private static final Pattern TWO_DIGIT_YEAR = Pattern
-            .compile("^TAF_(?<dd>\\d{2})(?<MM>\\d{2})(?<yy>\\d{2})_(?<hh>\\d{2})(?<mm>\\d{2})(?<ss>\\d{2})_\\d{8}\\.xml$");
-
-
-    @ParameterizedTest
-    @MethodSource("testArguments")
-    void match_filename_patterns(final String filename, final Pattern pattern, final ZonedDateTime expected) {
-        final FilenameMatcher matcher = new FilenameMatcher(filename, pattern, UTC);
-        final Optional<PartialOrCompleteTimeInstant> timestamp = matcher.getTimestamp(CLOCK);
-        assertThat(timestamp).isPresent();
-        final Optional<ZonedDateTime> completeTime = timestamp.get().getCompleteTime();
-        assertThat(completeTime).hasValue(expected);
-    }
-
-    private static Stream<Arguments> testArguments() {
+    static Stream<Arguments> timestampTestArguments() {
         return Stream.of(
                 // Four digit year
                 Arguments.of("TAF_13011965_024107_00007194.xml", FOUR_DIGIT_YEAR, ZonedDateTime.parse("1965-01-13T02:41:07Z")),
@@ -52,8 +41,38 @@ public class FilenameMatcherTest {
                 Arguments.of("TAF_130122_024107_00007194.xml", TWO_DIGIT_YEAR, ZonedDateTime.parse("2022-01-13T02:41:07Z")),
                 Arguments.of("TAF_130123_024107_00007194.xml", TWO_DIGIT_YEAR, ZonedDateTime.parse("2023-01-13T02:41:07Z")),
                 Arguments.of("TAF_130124_024107_00007194.xml", TWO_DIGIT_YEAR, ZonedDateTime.parse("1924-01-13T02:41:07Z")),
-                Arguments.of("TAF_130199_024107_00007194.xml", TWO_DIGIT_YEAR, ZonedDateTime.parse("1999-01-13T02:41:07Z"))
-        );
+                Arguments.of("TAF_130199_024107_00007194.xml", TWO_DIGIT_YEAR, ZonedDateTime.parse("1999-01-13T02:41:07Z")));
     }
 
+    @ParameterizedTest
+    @MethodSource("timestampTestArguments")
+    void getTimestamp_matches_filename_patterns(final String filename, final Pattern pattern, final ZonedDateTime expected) {
+        final FilenameMatcher matcher = new FilenameMatcher(filename, pattern, ZoneOffset.UTC);
+        final Optional<PartialOrCompleteTimeInstant> timestamp = matcher.getTimestamp(CLOCK);
+        assertThat(timestamp).isPresent();
+        final Optional<ZonedDateTime> completeTime = timestamp.get().getCompleteTime();
+        assertThat(completeTime).hasValue(expected);
+    }
+
+    @Test
+    void getTimestamp_resolves_forward_in_matcher_timezone() {
+        final Clock clock = Clock.fixed(Instant.parse("2000-12-31T22:34:56Z"), ZoneOffset.ofHours(-3));
+        final FilenameMatcher matcher = new FilenameMatcher("TAF_311202_213245_00007194.xml", TWO_DIGIT_YEAR, ZoneOffset.ofHours(2));
+
+        final Optional<PartialOrCompleteTimeInstant> timestamp = matcher.getTimestamp(clock);
+        assertThat(timestamp).isPresent();
+        final Optional<ZonedDateTime> completeTime = timestamp.get().getCompleteTime();
+        assertThat(completeTime).hasValue(ZonedDateTime.parse("2002-12-31T21:32:45+02:00"));
+    }
+
+    @Test
+    void getTimestamp_resolves_backward_in_matcher_timezone() {
+        final Clock clock = Clock.fixed(Instant.parse("2000-01-01T01:23:45Z"), ZoneOffset.ofHours(2));
+        final FilenameMatcher matcher = new FilenameMatcher("TAF_030201_213245_00007194.xml", TWO_DIGIT_YEAR, ZoneOffset.ofHours(-3));
+
+        final Optional<PartialOrCompleteTimeInstant> timestamp = matcher.getTimestamp(clock);
+        assertThat(timestamp).isPresent();
+        final Optional<ZonedDateTime> completeTime = timestamp.get().getCompleteTime();
+        assertThat(completeTime).hasValue(ZonedDateTime.parse("1901-02-03T21:32:45-03:00"));
+    }
 }
