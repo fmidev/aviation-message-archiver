1bf3ae4645c55618e1e666801853d880ee817040
Petri Vuorio <21276470+petringo@users.noreply.github.com>
2025-08-08 15:13:56 +0300

    SDM-2642: Remove redundant implements interface.

--- src/main/java/fi/fmi/avi/archiver/spring/convert/MapValuesToCollectionConverter.java
+++ src/main/java/fi/fmi/avi/archiver/spring/convert/MapValuesToCollectionConverter.java
@@ -1,17 +1,16 @@
 package fi.fmi.avi.archiver.spring.convert;
 
-import static java.util.Objects.requireNonNull;
+import org.springframework.core.convert.ConversionService;
+import org.springframework.core.convert.TypeDescriptor;
+import org.springframework.core.convert.converter.ConditionalGenericConverter;
+import org.springframework.core.convert.support.DefaultConversionService;
 
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Map;
 import java.util.Set;
 
-import org.springframework.core.convert.ConversionService;
-import org.springframework.core.convert.TypeDescriptor;
-import org.springframework.core.convert.converter.ConditionalGenericConverter;
-import org.springframework.core.convert.converter.GenericConverter;
-import org.springframework.core.convert.support.DefaultConversionService;
+import static java.util.Objects.requireNonNull;
 
 /**
  * A Converter converting values of provided Map to a supported collection, converting the elements as well.
@@ -20,7 +19,7 @@ import org.springframework.core.convert.support.DefaultConversionService;
  * This implementation uses internally a {@link ConversionService} to convert {@link Map#values()} collection into target collection.
  * </p>
  */
-public class MapValuesToCollectionConverter implements GenericConverter, ConditionalGenericConverter, ConversionServiceAware {
+public class MapValuesToCollectionConverter implements ConditionalGenericConverter, ConversionServiceAware {
     private static final Set<ConvertiblePair> CONVERTIBLE_TYPES = Collections.singleton(new ConvertiblePair(Map.class, Collection.class));
     private ConversionService conversionService;
 
@@ -36,8 +35,7 @@ public class MapValuesToCollectionConverter implements GenericConverter, Conditi
      * Constructs a new instance using provided {@code ConversionService} for conversions.
      * Typically, the new instance should be added to provided ConversionService to enable recursive conversions.
      *
-     * @param conversionService
-     *         conversion service to be used for value conversions
+     * @param conversionService conversion service to be used for value conversions
      */
     public MapValuesToCollectionConverter(final ConversionService conversionService) {
         this.conversionService = requireNonNull(conversionService, "conversionService");
