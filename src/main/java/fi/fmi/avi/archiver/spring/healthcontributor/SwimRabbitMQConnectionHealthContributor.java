bfbc3625d8e2661791ac4c7cb557aba449dc0b96
Topi Kasari <49980037+kasarit@users.noreply.github.com>
2025-09-02 14:49:30 +0300

    Fix running JUnit 4 tests (#140)

--- src/main/java/fi/fmi/avi/archiver/spring/healthcontributor/SwimRabbitMQConnectionHealthContributor.java
+++ src/main/java/fi/fmi/avi/archiver/spring/healthcontributor/SwimRabbitMQConnectionHealthContributor.java
@@ -1,10 +1,15 @@
 package fi.fmi.avi.archiver.spring.healthcontributor;
 
+import static java.util.Objects.requireNonNull;
+
 public class SwimRabbitMQConnectionHealthContributor extends RegistryCompositeHealthContributor {
 
     public void registerIndicators(final String id,
                                    final RabbitMQConnectionHealthIndicator connectionIndicator,
                                    final RabbitMQPublisherHealthIndicator publisherIndicator) {
+        requireNonNull(id, "id");
+        requireNonNull(connectionIndicator, "connectionIndicator");
+        requireNonNull(publisherIndicator, "publisherIndicator");
         registerContributor(id + ".connection", connectionIndicator);
         registerContributor(id + ".publisher", publisherIndicator);
     }
