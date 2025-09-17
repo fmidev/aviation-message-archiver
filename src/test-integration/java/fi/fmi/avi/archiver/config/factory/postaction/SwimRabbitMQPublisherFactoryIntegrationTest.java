036b446f09d2f2f3b609968459d771c393c2c9d6
kasarit <49980037+kasarit@users.noreply.github.com>
2025-09-17 17:38:08 +0300

    SDM-2639: Fill in minimal factory integration test

--- src/test-integration/java/fi/fmi/avi/archiver/config/factory/postaction/SwimRabbitMQPublisherFactoryIntegrationTest.java
+++ src/test-integration/java/fi/fmi/avi/archiver/config/factory/postaction/SwimRabbitMQPublisherFactoryIntegrationTest.java
@@ -2,26 +2,59 @@ package fi.fmi.avi.archiver.config.factory.postaction;
 
 import fi.fmi.avi.archiver.AviationMessageArchiver;
 import fi.fmi.avi.archiver.config.ConversionConfig;
+import fi.fmi.avi.archiver.config.PostActionFactoryConfig;
 import fi.fmi.avi.archiver.config.TestConfig;
+import fi.fmi.avi.archiver.config.model.PostActionFactory;
+import fi.fmi.avi.archiver.message.processor.postaction.PostAction;
+import fi.fmi.avi.archiver.message.processor.postaction.SwimRabbitMQPublisher;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.ConfigDataApplicationContextInitializer;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.support.AnnotationConfigContextLoader;
 
-@SpringBootTest({"auto.startup=false", "testclass.name=fi.fmi.avi.archiver.config.factory.postaction.SwimRabbitMQPublisherFactoryIntegrationTest"})
-@ContextConfiguration(classes = {AviationMessageArchiver.class, TestConfig.class, ConversionConfig.class},
-        loader = AnnotationConfigContextLoader.class,
+import java.util.List;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@SpringBootTest({
+        "auto.startup=false",
+        "testclass.name=fi.fmi.avi.archiver.config.factory.postaction.SwimRabbitMQPublisherFactoryIntegrationTest"
+})
+@ContextConfiguration(classes = {
+        AviationMessageArchiver.class,
+        TestConfig.class,
+        ConversionConfig.class,
+        PostActionFactoryConfig.class
+}, loader = AnnotationConfigContextLoader.class,
         initializers = {ConfigDataApplicationContextInitializer.class})
-@Sql(scripts = {"classpath:/fi/fmi/avi/avidb/schema/h2/schema-h2.sql", "classpath:/h2-data/avidb_test_content.sql"}, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
-@Sql(scripts = "classpath:/h2-data/avidb_cleanup_test.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
 @ActiveProfiles({"integration-test", "SwimRabbitMQPublisherFactoryIntegrationTest"})
 class SwimRabbitMQPublisherFactoryIntegrationTest {
+
+    @SpyBean(name = "swimRabbitMQPublisherPostActionFactory")
+    private PostActionFactory<SwimRabbitMQPublisher> factory;
+
+    @Autowired
+    private List<PostAction> postActions;
+
     @Test
-    void test() {
-        // instantiation succeeds if no exceptions
-        // TODO: proper tests as part of SDM-2639
+    void test_config_instantiation() {
+        @SuppressWarnings("unchecked") final ArgumentCaptor<Map<String, Object>> configCaptor = ArgumentCaptor.forClass(Map.class);
+        verify(factory, times(1)).newInstance(configCaptor.capture());
+
+        final Map<String, Object> config = configCaptor.getValue();
+        assertThat(config.get("id")).isEqualTo("test-id");
+
+        assertThat(postActions).hasSize(1);
+        final PostAction postAction = postActions.getFirst();
+        assertThat(postAction).isInstanceOf(SwimRabbitMQPublisher.class);
+        assertThat(postAction.toString()).isEqualTo("SwimRabbitMQPublisher(test-id)");
     }
 }
